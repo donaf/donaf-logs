594a17ba666427e84525932a563881ef1100f44a
donaf <2420639180@qq.com>
2022-07-03 16:56:21 +0800

    feat: 处理响应data

--- ts/ts-axios/src/helpers/data.ts
+++ ts/ts-axios/src/helpers/data.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: qf
  * @Date: 2022-06-13 18:48:56
- * @LastEditTime: 2022-07-03 13:31:52
+ * @LastEditTime: 2022-07-03 16:49:34
  * @LastEditors: qf
  * @Description:
  */
@@ -10,6 +10,23 @@ import { isPlainObject } from './util'
 export function transformRequest(data: any): any {
   if (isPlainObject(data)) {
     return JSON.stringify(data)
+  }
+  return data
+}
+
+/**
+ * 处理相应data
+ * @description 当服务端返回给我们的数据是字符串类型，我们可以尝试去把它转换成一个 JSON 对象
+ * @param data
+ * @returns
+ */
+export function transformResponse(data: any): any {
+  if (typeof data === 'string') {
+    try {
+      data = JSON.parse(data)
+    } catch (e) {
+      //
+    }
   }
   return data
 }
--- ts/ts-axios/src/index.ts
+++ ts/ts-axios/src/index.ts
@@ -1,22 +1,25 @@
 /*
  * @Author: qf
  * @Date: 2022-05-27 10:24:31
- * @LastEditTime: 2022-07-03 16:40:10
+ * @LastEditTime: 2022-07-03 16:55:31
  * @LastEditors: qf
  * @Description:
  */
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 import { bulidURL } from './helpers/url'
-import { transformRequest } from './helpers/data'
+import { transformRequest, transformResponse } from './helpers/data'
 import { processHeaders } from './helpers/headers'
-import { AxiosPromise } from './types/index'
+import { AxiosPromise, AxiosResponse } from './types/index'
 
 // 整个库的入口文件
 function axios(config: AxiosRequestConfig): AxiosPromise {
   // 发送请求逻辑,模块化的编程思想，把这个功能拆分到一个单独的模块中。
   processConfig(config)
-  return xhr(config)
+  return xhr(config).then(res => {
+    // 把响应的 data 字段从字符串解析成 JSON 对象结构
+    return transformResponseData(res)
+  })
 }
 
 function processConfig(config: AxiosRequestConfig): void {
@@ -30,13 +33,31 @@ function transformUrl(config: AxiosRequestConfig): string {
   return bulidURL(url, params)
 }
 
+/**
+ * 处理请求headers
+ */
 function transformHeaders(config: AxiosRequestConfig) {
   const { headers = {}, data } = config
   return processHeaders(headers, data)
 }
 
+/**
+ * 处理请求data
+ * @param config
+ * @returns
+ */
 function transformRequestData(config: AxiosRequestConfig): any {
   return transformRequest(config.data)
+}
+
+/**
+ * 处理相应data
+ * @param res 把响应的 data 字段从字符串解析成 JSON 对象结构
+ * @returns
+ */
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+  res.data = transformResponse(res.data)
+  return res
 }
 
 export default axios
